7b72c643e179098ed15178490beda4b14be0b7b7
vikas <vikas18687@gmail.com>
2020-03-05 19:24:01 +0530

    added solutions for abbreviate-string and longest-word-in-string

--- js-exercises/longest-word-in-string/longestWordInString.js
+++ js-exercises/longest-word-in-string/longestWordInString.js
@@ -1,5 +1,12 @@
 function longestWordInString(...args) {
-  return args;
+  let arrayOfwords = args[0].split(' ');
+  let longestWordLength = 0;
+  arrayOfwords.forEach(singleWord => {
+    if (singleWord.length > longestWordLength) {
+      longestWordLength = singleWord.length;
+    }
+  });
+  return longestWordLength;
 }
 
 export { longestWordInString };
