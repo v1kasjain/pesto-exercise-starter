97f875e0eb5e6790662b73a781d57d6c544d30a9
vikas <vikas18687@gmail.com>
2020-03-06 19:23:21 +0530

    solution for accountDataset question

--- js-exercises/account-dataset/accountDataset.js
+++ js-exercises/account-dataset/accountDataset.js
@@ -8,15 +8,65 @@ const fileContent = fs.readFileSync(
 
 const { bankBalances } = JSON.parse(fileContent);
 
-function hundredThousandairs() {}
+function hundredThousandairs() {
+  let arrayoFAccounts = [];
+  for (let account of bankBalances) {
+    if (parseFloat(account.amount) > 100000) {
+      arrayoFAccounts.push(account)
+    }
+  }
+  return arrayoFAccounts;
+}
 
-function datasetWithRoundedDollar() {}
+function datasetWithRoundedDollar() {
+  let bankBalanceswithRoundedDollar = [];
+  for (let account of bankBalances) {
+    account['rounded'] = Math.round(account.amount);
+    bankBalanceswithRoundedDollar.push(account);
+  }
+  return bankBalanceswithRoundedDollar;
+}
 
-function sumOfBankBalances() {}
+function sumOfBankBalances() {
+  let sumOfBalance = 0;
+  for (let account of bankBalances) {
+    sumOfBalance += parseFloat(account.amount);
+  }
+  return parseFloat(sumOfBalance.toFixed(2));
+}
 
-function sumOfInterests() {}
+function sumOfInterests() {
+  let states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+  let sumOfInt = 0;
+  for (let account of bankBalances) {
+    if (states.indexOf(account.state) > -1) {
+      let amount =  parseFloat(account.amount);
+      let interest = amount * .189;
+      sumOfInt +=  interest;
+    }
+  }
+  return sumOfInt.toFixed(2);
+}
 
-function higherStateSums() {}
+function higherStateSums() {
+  let sumOfAmountStateWise = {};
+  for (let account of bankBalances) {
+    if (sumOfAmountStateWise[account.state]) {
+      sumOfAmountStateWise[account.state] += parseFloat(account.amount);
+    } else {
+      sumOfAmountStateWise[account.state] = parseFloat(account.amount);
+    }
+  }
+
+  let sumOfStateAmount = 0;
+  Object.keys(sumOfAmountStateWise).forEach(key => {
+    if (sumOfAmountStateWise[key] > 1000000) {
+      sumOfStateAmount += sumOfAmountStateWise[key];
+    }
+  });
+
+  return sumOfStateAmount;
+ }
 
 export {
   hundredThousandairs,
--- js-exercises/account-dataset/accountDataset.test.js
+++ js-exercises/account-dataset/accountDataset.test.js
@@ -43,7 +43,7 @@ describe('accountDataset', () => {
     });
   });
 
-  describe.only('sumOfInterests', () => {
+  describe('sumOfInterests', () => {
     it('should return the sum the 18.9% interest for all amounts in bankBalances, in the selected states, rounded to the nearest cent', () => {
       expect(sumOfInterests())
         .toBe(2504611.23);
